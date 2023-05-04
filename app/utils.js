7fbaa22263e6f054ffc379be15a56966097790ff
labrac <raquelortegalabrac@hotmail.com>
2023-05-04 15:57:09 +0200

    Add testing exercise 2

--- app/utils.js
+++ app/utils.js
@@ -1,15 +1,6 @@
-// Const checkVowelsInAWord = (word) => {
-//   const vowels = ["a"];
-//   return vowels.every((vowel) => word.includes(vowel));
-// };
-
-// checkVowelsInAWord("hola");
-
 export const checkIfHasVowels = (word) => {
   const vowels = ["a", "e", "i", "o", "u"];
   return vowels.every((vowel) => word.includes(vowel));
 };
 
-export default checkIfHasVowels;
-
-export const IsEven = (number) => Object.is(number % 2, 0);
+export const isEven = (number) => Object.is(number % 2, 0);
